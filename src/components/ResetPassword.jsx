f18ff3990a72a5c82682684201f50006a9ff2872
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-08-29 18:02:25 +0530

    Update ResetPassword.jsx

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -20,7 +20,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/reset-password', {
+      const response = await fetch('https://e-commerce-website-project.onrender.com/reset-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
