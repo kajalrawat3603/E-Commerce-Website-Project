420e07771dcdca8e91923889ab4c80ac8ae5ed3e
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-08-29 18:02:06 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -13,7 +13,7 @@ const Product = () => {
   useEffect(() => {
     const fetchProductDetails = async () => {
   try {
-    const response = await fetch(`http://localhost:5000/products/${productId}`, {
+    const response = await fetch(`https://e-commerce-website-project.onrender.com/products/${productId}`, {
       method: 'GET'
     });
     if (!response.ok) {
@@ -69,7 +69,7 @@ const Product = () => {
     };
 
     try {
-        const response = await fetch('http://localhost:5000/cart/add', {
+        const response = await fetch('https://e-commerce-website-project.onrender.com/cart/add', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
