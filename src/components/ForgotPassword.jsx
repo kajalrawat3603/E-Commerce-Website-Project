58c88dd2c3583c7fad0536110d844ac5d9f06624
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-08-29 17:59:50 +0530

    Update ForgotPassword.jsx

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -10,7 +10,7 @@ const ForgotPassword = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/forgot-password', {
+      const response = await fetch('https://e-commerce-website-project.onrender.com/forgot-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
